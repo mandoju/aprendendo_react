be446842210287ce176fbbc836ef9c86063d544e
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2018-01-15 06:59:29 -0200

    criando árvore de importação

---
+++ exercicios_react/ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+     <div> 
+         <Member name='Jorge' lastName='Mandoju' />
+         <Member name='Jorge' lastName='Rama' />
+         <Member name='Júlio' lastName='Rama' />
+         <Member name='Ivana' lastName='Mandoju' />
+    </div>
+)
