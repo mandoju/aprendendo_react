0f82e8d296dcac47abed703bd1e28f6fde5ebdd8
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2018-01-15 03:54:21 -0200

    Olá react

---
+++ exercicios_react/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            }
+        ]
+    }
+}
