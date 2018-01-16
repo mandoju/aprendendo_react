f959fd71624cf648c6f14763c316c839852225f6
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2018-01-16 02:21:26 -0200

    Configuração das dependências no frontend

---
+++ todo-app/frontend/webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+
+    },
+    resolve: {
+        extensions: ['.js','.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        rules: [
+            {
+              test: /.js[x]?$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['es2015','react'],
+                  plugins: ['transform-object-rest-spread']
+                }
+              }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+                
+            },
+            {
+                test: /\.woff|.woff2|.tff|.eot|.svg*.*$/,
+                use: {
+                      loader: 'file-loader',
+                      options: {}  
+                }
+                
+            }
+        ]
+    }
+}
